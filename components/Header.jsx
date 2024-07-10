331e9975621da2f9c9a350c872356c7fc0b5c854
Raees <mraiskundi@gmail.com>
2024-07-10 17:14:45 +0500

    Header update

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import logo from "../assets/logo.png";
 import { FaSun } from "react-icons/fa6";
 import { FaCartShopping } from "react-icons/fa6";
+import { FaMagnifyingGlass } from "react-icons/fa6";
 
 import { FaMoon } from "react-icons/fa6";
 
@@ -9,7 +10,7 @@ const Header = () => {
   const [ordertext, setOrderText] = useState(false);
   const [isDark, setisDark] = useState(false);
   return (
-    <div className="flex justify-between items-center bg-[#FFDCA9] px-10 py-2 main-header">
+    <div className="flex justify-between items-center bg-[#FFDCA9] px-10 py-1 main-header">
       {/* header logo section  */}
       <div className="left-header flex items-center">
         {" "}
@@ -18,15 +19,17 @@ const Header = () => {
       </div>
       {/* Header searchBar section  */}
       <div className="right-header flex items-center">
-        <div className="flex items-center border-2 rounded-full overflow-hidden searchBar">
+        <div className="flex items-center border-2 bg-white rounded-full min-h-10 overflow-hidden searchBar">
           {" "}
           <input
             type="search"
-            className="px-4 outline-none"
+            className="px-4 max-w-48 outline-none"
             placeholder="search"
             id="searchBar"
           ></input>
-          <button className="bg-red-400">Searchs</button>
+          <button className="bg-white px-3 text-gray-400">
+            <FaMagnifyingGlass />
+          </button>
         </div>
         <div
           className="flex items-center bg-[#F49510] mx-3 px-5 rounded-full h-8 text-white"
