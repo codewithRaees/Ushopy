cbb20fb62c3ed76f16737b32e0e78dccb42f10b5
Raees <mraiskundi@gmail.com>
2024-07-10 16:52:06 +0500

    Initial commit

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../assets/logo.png";
-import { IconName } from "react-icons/fa6";
+import { FaSun } from "react-icons/fa6";
+import { FaCartShopping } from "react-icons/fa6";
+
+import { FaMoon } from "react-icons/fa6";
 
 const Header = () => {
+  const [ordertext, setOrderText] = useState(false);
+  const [isDark, setisDark] = useState(false);
   return (
-    <div className="flex justify-around items-center main-header">
+    <div className="flex justify-between items-center bg-[#FFDCA9] px-10 py-2 main-header">
       {/* header logo section  */}
       <div className="left-header flex items-center">
         {" "}
@@ -12,17 +17,42 @@ const Header = () => {
         <h1 className="font-bold text-2xl">Ushopy</h1>
       </div>
       {/* Header searchBar section  */}
-      <div className="right-header">
-        <div className="searchBar">
+      <div className="right-header flex items-center">
+        <div className="flex items-center border-2 rounded-full overflow-hidden searchBar">
           {" "}
           <input
             type="search"
-            className="border-2 outline-none"
+            className="px-4 outline-none"
             placeholder="search"
             id="searchBar"
           ></input>
-          <FaBeer />
+          <button className="bg-red-400">Search</button>
         </div>
+        <div
+          className="flex items-center bg-[#F49510] mx-3 px-5 rounded-full h-8 text-white"
+          onMouseOver={() => {
+            setOrderText(!ordertext);
+          }}
+        >
+          <span
+            className={`transition duration-1000 ease-in-out    ${
+              !ordertext ? "inline" : "hidden"
+            } px-2`}
+          >
+            Orders
+          </span>
+          <FaCartShopping className="text-2xl text-white" />
+        </div>
+        <p
+          onClick={() => {
+            setisDark(!isDark);
+            console.log(!isDark);
+          }}
+          className={` bg-green-400 flex items-center `}
+        >
+          {!isDark ? <FaSun /> : <FaMoon />}
+          {!isDark ? "Dark" : "light"} Mode
+        </p>
       </div>
     </div>
   );
